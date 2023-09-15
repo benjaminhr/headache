0075221c93305c771454ae15b1aea714785685b0
benjaminhr <robsonhbenjamin@gmail.com>
2023-09-15 15:40:55 +0300

    bauhaus grain

--- rects/main.js
+++ rects/main.js
@@ -9,31 +9,55 @@ const randomNumber = (min, max) => {
 
 const colours = ["#040D12", "#183D3D", "#5C8374", "#93B1A6"];
 
-let prevX = 100;
-let prevY = 0;
-let prevWidth = 0;
-let prevHeight = 0;
+const addGrain = () => {
+  const canvasImage = context.getImageData(0, 0, canvas.width, canvas.height);
+  const pixels = canvasImage.data;
+  const pixelCount = 4 * canvas.width * canvas.height;
+  const amount = 15;
 
-for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < pixelCount; i += 4) {
+    const grainAmount = Math.round(
+      Math.random() * (amount - -amount) + -amount
+    );
+    pixels[i] = pixels[i] + grainAmount;
+    pixels[i + 1] = pixels[i + 1] + grainAmount;
+    pixels[i + 2] = pixels[i + 2] + grainAmount;
+  }
+  const image = new ImageData(pixels, canvas.width, canvas.height);
+  context.putImageData(image, 0, 0);
+};
+
+const animate = () => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
   context.beginPath();
-  context.save();
-  context.globalAlpha = randomNumber(0.1, 1);
-
-  context.setLineDash([randomNumber(0, 20), randomNumber(0, 20)]);
-  context.strokeStyle = "black";
-  context.lineWidth = 10;
-  context.fillStyle = colours[Math.floor(Math.random() * colours.length)];
-  const x = randomNumber(0, canvas.width - 100);
-  const y = randomNumber(0, canvas.height - 100);
-  const width = randomNumber(10, canvas.width);
-  const height = randomNumber(10, canvas.height);
-  prevX = x;
-  prevY = y;
-  prevWidth = width;
-  prevHeight = height;
-
+  let x = 100;
+  let y = 100;
+  let width = canvas.width - 200;
+  let height = canvas.height - 200;
+  context.fillStyle = colours[1];
   context.rect(x, y, width, height);
   context.fill();
   context.stroke();
-  context.restore();
-}
+
+  for (let i = 0; i < 30; i++) {
+    context.beginPath();
+    context.globalAlpha = randomNumber(0.1, 1);
+    // context.setLineDash([randomNumber(0, 20), randomNumber(0, 20)]);
+    // context.strokeStyle = "black";
+    context.lineWidth = 5;
+    context.fillStyle = colours[Math.floor(Math.random() * colours.length)];
+
+    const newX = randomNumber(x, x + width);
+    const newY = randomNumber(y, y + height);
+    const newWidth = randomNumber(50, width - newX);
+    const newHeight = randomNumber(50, height - newY);
+    context.rect(newX, newY, newWidth, newHeight);
+    context.fill();
+    context.stroke();
+  }
+
+  addGrain();
+};
+
+setInterval(animate, 2000);
